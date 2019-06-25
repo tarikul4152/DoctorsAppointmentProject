61c16714d08fd2dfd144085a42f9c42e63d40da2
tarikul4152 <33080982+tarikul4152@users.noreply.github.com>
2019-06-25 18:38:58 +0600

    dependencies added

--- app/src/main/java/com/tarikul4152/doctorsappointment/WelcomeActivity.java
+++ app/src/main/java/com/tarikul4152/doctorsappointment/WelcomeActivity.java
@@ -1,14 +1,32 @@
 package com.tarikul4152.doctorsappointment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 public class WelcomeActivity extends AppCompatActivity
 {
+    Thread thread;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        thread=new Thread(new Runnable() {
+            @Override
+            public void run()
+            {
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Intent i=new Intent(WelcomeActivity.this,MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+        thread.start();
     }
 }
