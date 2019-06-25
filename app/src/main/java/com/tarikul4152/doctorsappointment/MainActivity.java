31c2565facb1e7df8c915224aca9095c77105554
tarikul4152 <33080982+tarikul4152@users.noreply.github.com>
2019-06-26 02:02:25 +0600

    dependencies added

--- app/src/main/java/com/tarikul4152/doctorsappointment/MainActivity.java
+++ app/src/main/java/com/tarikul4152/doctorsappointment/MainActivity.java
@@ -3,6 +3,7 @@ package com.tarikul4152.doctorsappointment;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.support.v4.view.GravityCompat;
 import android.support.v7.app.ActionBarDrawerToggle;
