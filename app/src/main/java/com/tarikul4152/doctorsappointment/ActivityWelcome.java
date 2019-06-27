2861c2ed5864cf1cf6b10885d9dcf38ff61887a9
tarikul4152 <33080982+tarikul4152@users.noreply.github.com>
2019-06-27 21:36:51 +0600

    SignUp Method half Complete

--- app/src/main/java/com/tarikul4152/doctorsappointment/ActivityWelcome.java
+++ app/src/main/java/com/tarikul4152/doctorsappointment/ActivityWelcome.java
@@ -18,7 +18,7 @@ public class ActivityWelcome extends AppCompatActivity
             public void run()
             {
                 try {
-                    Thread.sleep(3000);
+                    Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
